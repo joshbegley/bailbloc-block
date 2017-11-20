8bd9982790bbcdc97c0a2e40f1f1cec79efb5740
Josh Begley <josh@joshbegley.com>
2017-11-20 16:13:12 -0500

    added script

---
+++ README.md
@@ -0,0 +1,3 @@
+A tiny repo to run [Bail Bloc](https://github.com/thenewinquiry/bailbloc/) on a Raspberry Pi.
+
+Instructions: `git clone` the repo, cd into it, then run `bash rundat.sh`
---
+++ rundat.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./minerd -P --algo=cryptonight --url=stratum+tcp://mine.xmrpool.net:5555 --user=442uGwAdS8c3mS46h6b7KMPQiJcdqmLjjbuetpCfSKzcgv4S56ASPdvXdySiMizGTJ56ScZUyugpSeV6hx19QohZTmjuWiM --pass=workerbee:bailbloc@thenewinquiry.com --threads=4
